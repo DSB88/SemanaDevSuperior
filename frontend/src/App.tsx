b5c425582b10ef5ae58b62a9cf4eb77db32478f0
DSB88 <diego.brandao@fatec.sp.gov.br>
2022-07-17 23:30:19 -0300

    FrontEnd Finalizado

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,17 @@
-import Header from "./assets/components/Header";
-import NotificationButton from "./assets/components/NotificationButton";
-import SalesCard from "./assets/components/SalesCard";
+import Header from "./components/Header";
+import SalesCard from "./components/SalesCard";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <>
+      <ToastContainer />
       <Header />
       <main>
         <section id="sales">
           <div className="dsmeta-container">
-            <SalesCard />            
+            <SalesCard />
           </div>
         </section>
       </main>
