f64fe85dc46c45c16693375e0444d6e0c89d2df2
DSB88 <diego.brandao@fatec.sp.gov.br>
2022-07-19 17:27:27 -0300

    Ajustando biblioteca

--- backend/src/main/java/com/devsuperior/DevsuperiorApplication.java
+++ backend/src/main/java/com/devsuperior/DevsuperiorApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
 @SpringBootApplication
 public class DevsuperiorApplication {
 
@@ -13,8 +13,4 @@ public class DevsuperiorApplication {
 		SpringApplication.run(DevsuperiorApplication.class, args);
 	}
 
-	@GetMapping("/home")
-	public String home(){
-		return "Olá Mundo";
-	}
 }
